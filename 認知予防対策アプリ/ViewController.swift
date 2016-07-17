53b081cf2e22d7d359336124ff0c2a4db3e29fa6
スズコー <suzukou@suzukisadayoshi-no-MacBook.local>
2016-07-17 15:53:28 +0900

    アルファ版

---
+++ 認知予防対策アプリ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  認知予防対策アプリ
+//
+//  Created by スズコー on 2016/06/05.
+//  Copyright © 2016年 スズコー. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
